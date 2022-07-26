ddfde46114dc3968301fab9a42637406a36b8183
geraldodf <geraldodarosfilho@hotmail.com>
2022-07-26 08:39:40 -0300

    First commit

---
+++ src/pages/index.jsx
@@ -0,0 +1,10 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import MenuLateral from './../components/MenuLateral'
+
+export default function Home() {
+  return (
+    <>
+      <MenuLateral></MenuLateral>
+    </>
+  )
+}
