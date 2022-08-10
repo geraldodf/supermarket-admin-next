787c5a7bbc08001078d39015926f9fe997f70ef7
geraldodf <geraldodarosfilho@hotmail.com>
2022-08-10 11:04:23 -0300

    Refatorando classe Produto

--- src/core/Produto.ts
+++ src/core/Produto.ts
@@ -1,9 +1,66 @@
 export default class Produto {
-    private id: number
-    private descricao: string
-    private codigo: number
-    private precoDeCompra: number
-    private precoDeVenda: number
-    private quantidade: number
-    private tipoDoProduto: number
+    #id: number
+    #descricao: string
+    #codigo: number
+    #precoDeCompra: number
+    #precoDeVenda: number
+    #quantidade: number
+    #tipoDoProduto: number
+
+
+    get id(): number {
+        return this.#id;
+    }
+
+    set id(value: number) {
+        this.#id = value;
+    }
+
+    get descricao(): string {
+        return this.#descricao;
+    }
+
+    set descricao(value: string) {
+        this.#descricao = value;
+    }
+
+    get codigo(): number {
+        return this.#codigo;
+    }
+
+    set codigo(value: number) {
+        this.#codigo = value;
+    }
+
+    get precoDeCompra(): number {
+        return this.#precoDeCompra;
+    }
+
+    set precoDeCompra(value: number) {
+        this.#precoDeCompra = value;
+    }
+
+    get precoDeVenda(): number {
+        return this.#precoDeVenda;
+    }
+
+    set precoDeVenda(value: number) {
+        this.#precoDeVenda = value;
+    }
+
+    get quantidade(): number {
+        return this.#quantidade;
+    }
+
+    set quantidade(value: number) {
+        this.#quantidade = value;
+    }
+
+    get tipoDoProduto(): number {
+        return this.#tipoDoProduto;
+    }
+
+    set tipoDoProduto(value: number) {
+        this.#tipoDoProduto = value;
+    }
 }
